506ca640c3156a861f560dcb31c32f29eaf94a0e
兮尘 <23211526+LinXueyuanStdio@users.noreply.github.com>
2024-06-16 18:54:00 +0800

    save

--- st_diff_viewer/frontend/components/streamlit-diff-viewer.tsx
+++ st_diff_viewer/frontend/components/streamlit-diff-viewer.tsx
@@ -1,25 +1,31 @@
 import { useRenderData } from "streamlit-component-lib-react-hooks";
+import { Streamlit } from "streamlit-component-lib";
 import React from 'react';
-import ReactDiffViewer from 'react-diff-viewer';
+import ReactDiffViewer from 'react-diff-viewer-continued';
+import ObserveSize from 'react-observe-size';
 
 function StreamlitDiffViewer() {
   const { theme, disabled, args } = useRenderData();
 
   return (
-    <div>
-      <ReactDiffViewer
-        oldValue={args.oldText}
-        newValue={args.newText}
-        splitView={args.splitView}
-        disableWordDiff={args.disabledWordDiff}
-        leftTitle={args.leftTitle}
-        rightTitle={args.rightTitle}
-        useDarkTheme={args.useDarkTheme}
-        extraLinesSurroundingDiff={args.extraLinesSurroundingDiff}
-        hideLineNumbers={args.hideLineNumbers}
-        highlightLines={args.highlightLines}
-      />
-    </div>
+    <ObserveSize observerFn={(contentRect) => {
+      Streamlit.setFrameHeight(contentRect.height);
+    }}>
+      <div>
+        <ReactDiffViewer
+          oldValue={args.oldText}
+          newValue={args.newText}
+          splitView={args.splitView}
+          disableWordDiff={args.disabledWordDiff}
+          leftTitle={args.leftTitle}
+          rightTitle={args.rightTitle}
+          useDarkTheme={args.useDarkTheme}
+          extraLinesSurroundingDiff={args.extraLinesSurroundingDiff}
+          hideLineNumbers={args.hideLineNumbers}
+          highlightLines={args.highlightLines}
+        />
+      </div>
+    </ObserveSize>
   );
 }
 
