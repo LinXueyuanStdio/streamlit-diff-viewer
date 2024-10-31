ce46884d12045d3ac28b4f924064ca3b6634877c
RedHitMark <russodivito.marco@gmail.com>
2024-10-31 18:08:52 +0100

    build

--- st_diff_viewer/frontend/out/_next/static/R8-sZqhKigDxjwG4kYFvh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/589-8d139740f609832d.js","static/chunks/pages/index-a75592537628c1d9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
